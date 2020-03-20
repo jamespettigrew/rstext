81620c0d0049427b9150e49e5612e0daf4a1389a
James Pettigrew <james@innovum.com.au>
2020-03-20 20:31:44 +1100

    Migrate editor code over to using piece table.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::io::{stdout, Write};
-
 mod piece_table;
+use piece_table::{ PieceTable, TextBuffer };
+use std::io::{stdout, Write};
 
 use crossterm::{
     cursor::MoveTo,
@@ -11,72 +11,6 @@ use crossterm::{
     terminal::{Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen},
     Result,
 };
-
-struct Line {
-    character_buffer: Vec<char>,
-}
-
-impl Line {
-    fn new() -> Line {
-        Line {
-            character_buffer: vec![],
-        }
-    }
-
-    fn insert_character_at(&mut self, character: char, column: usize) {
-        let column = column as usize;
-
-        if column <= self.character_buffer.len() {
-            self.character_buffer.insert(column, character);
-        }
-    }
-
-    fn remove_character_at(&mut self, column: usize) {
-        if column < self.character_buffer.len() {
-            self.character_buffer.remove(column);
-        }
-    }
-}
-
-struct LineBuffer {
-    lines: Vec<Line>,
-}
-
-impl LineBuffer {
-    fn new() -> LineBuffer {
-        LineBuffer {
-            lines: vec![Line::new()],
-        }
-    }
-
-    fn insert_line_at(&mut self, line: Line, row: usize) {
-        if row <= self.lines.len() {
-            self.lines.insert(row, line);
-        }
-    }
-
-    fn line_at(&self, row: usize) -> Option<&Line> {
-        if row < self.lines.len() {
-            return Some(&self.lines[row]);
-        }
-
-        None
-    }
-
-    fn line_at_mut(&mut self, row: usize) -> Option<&mut Line> {
-        if row < self.lines.len() {
-            return Some(&mut self.lines[row]);
-        }
-
-        None
-    }
-
-    fn remove_line_at(&mut self, row: usize) {
-        if row > 0 && row < self.lines.len() {
-            self.lines.remove(row);
-        }
-    }
-}
 
 struct Cursor {
     row: usize,
@@ -198,14 +132,14 @@ fn main() -> Result<()> {
     execute!(stdout(), EnterAlternateScreen)?;
     terminal::enable_raw_mode()?;
 
-    let line_buffer = &mut LineBuffer::new();
+    let text_buffer= &mut PieceTable::new(vec![]);
     let cursor = &mut Cursor::new();
     let window = &mut Window::new(0, 0, 0, 0);
 
     let screen = &mut stdout();
 
     loop {
-        render(screen, line_buffer, cursor, window);
+        render(screen, text_buffer, cursor, window);
 
         if let Ok(Event::Key(event)) = event::read() {
             match event {
@@ -218,17 +152,9 @@ fn main() -> Result<()> {
                     modifiers: _,
                 } => {
                     if cursor.column == 0 && cursor.row > 0 {
-                        let current_line = &mut line_buffer
-                            .line_at(cursor.row)
-                            .expect("Missing expected line.")
-                            .character_buffer
-                            .clone();
-                        let line_above = line_buffer
-                            .line_at_mut(cursor.row - 1)
-                            .expect("Missing expected line.");
-                        let new_cursor_column = line_above.character_buffer.len();
-                        line_above.character_buffer.append(current_line);
-                        line_buffer.remove_line_at(cursor.row);
+                        let line_above = text_buffer.line_at(cursor.row - 1);
+                        let new_cursor_column = line_above.content.len();
+                        text_buffer.remove_item_at(line_above.start_index + line_above.content.len());
 
                         let column_delta = (new_cursor_column as isize) - (cursor.column as isize);
                         let cursor_move = CursorMove {
@@ -237,26 +163,20 @@ fn main() -> Result<()> {
                         };
                         cursor.moved(cursor_move);
                     } else if cursor.column > 0 {
-                        let current_line = line_buffer
-                            .line_at_mut(cursor.row)
-                            .expect("Missing expected line.");
+                        let current_line = text_buffer.line_at(cursor.row);
                         cursor.moved(CursorMove {
                             vertical: None,
                             horizontal: Some(HorizontalMove::Left(1)),
                         });
-                        current_line.remove_character_at(cursor.column);
+                        text_buffer.remove_item_at(current_line.start_index + cursor.column);
                     }
                 }
                 KeyEvent {
                     code: KeyCode::Enter,
                     modifiers: _,
                 } => {
-                    let current_line = line_buffer
-                        .line_at_mut(cursor.row)
-                        .expect("Missing expected line.");
-                    let mut new_line = Line::new();
-                    let characters_to_eol = current_line.character_buffer.drain(cursor.column..);
-                    new_line.character_buffer.extend(characters_to_eol);
+                    let current_line = text_buffer.line_at(cursor.row);
+                    text_buffer.insert_item_at('\n', current_line.start_index + cursor.column);
 
                     let column_delta = 0 - (cursor.column as isize);
                     let cursor_move = CursorMove {
@@ -264,16 +184,13 @@ fn main() -> Result<()> {
                         horizontal: HorizontalMove::from_delta(column_delta),
                     };
                     cursor.moved(cursor_move);
-                    line_buffer.insert_line_at(new_line, cursor.row);
                 }
                 KeyEvent {
                     code: KeyCode::Char(c),
                     modifiers: _,
                 } => {
-                    let current_line = line_buffer
-                        .line_at_mut(cursor.row)
-                        .expect("Missing expected line.");
-                    current_line.insert_character_at(c, cursor.column);
+                    let current_line = text_buffer.line_at(cursor.row);
+                    text_buffer.insert_item_at(c, current_line.start_index + cursor.column);
                     let cursor_move = CursorMove {
                         vertical: None,
                         horizontal: Some(HorizontalMove::Right(1)),
@@ -285,14 +202,12 @@ fn main() -> Result<()> {
                     modifiers: _,
                 } => {
                     if cursor.row > 0 {
-                        let line_above = line_buffer
-                            .line_at(cursor.row - 1)
-                            .expect("Missing expected line.");
+                        let line_above = text_buffer.line_at(cursor.row - 1);
                         let mut column_delta = 0;
-                        if line_above.character_buffer.len() == 0
-                            || (line_above.character_buffer.len() - 1) < cursor.column
+                        if line_above.content.len() == 0
+                            || (line_above.content.len() - 1) < cursor.column
                         {
-                            column_delta = (line_above.character_buffer.len() as isize)
+                            column_delta = (line_above.content.len() as isize)
                                 - (cursor.column as isize)
                         }
 
@@ -307,15 +222,13 @@ fn main() -> Result<()> {
                     code: KeyCode::Down,
                     modifiers: _,
                 } => {
-                    if cursor.row < line_buffer.lines.len() - 1 {
-                        let line_below = line_buffer
-                            .line_at(cursor.row + 1)
-                            .expect("Missing expected line.");
+                    if cursor.row < text_buffer.line_count() - 1 {
+                        let line_below = text_buffer.line_at(cursor.row + 1);
                         let mut column_delta = 0;
-                        if line_below.character_buffer.len() == 0
-                            || (line_below.character_buffer.len() - 1) < cursor.column
+                        if line_below.content.len() == 0
+                            || (line_below.content.len() - 1) < cursor.column
                         {
-                            column_delta = (line_below.character_buffer.len() as isize)
+                            column_delta = (line_below.content.len() as isize)
                                 - (cursor.column as isize)
                         }
 
@@ -336,11 +249,9 @@ fn main() -> Result<()> {
                             horizontal: Some(HorizontalMove::Left(1)),
                         });
                     } else if cursor.row > 0 {
-                        let line_above = line_buffer
-                            .line_at(cursor.row - 1)
-                            .expect("Missing expected line.");
+                        let line_above = text_buffer.line_at(cursor.row - 1);
                         let column_delta =
-                            (line_above.character_buffer.len() as isize) - (cursor.column as isize);
+                            (line_above.content.len() as isize) - (cursor.column as isize);
                         let cursor_move = CursorMove {
                             vertical: Some(VerticalMove::Up(1)),
                             horizontal: HorizontalMove::from_delta(column_delta),
@@ -352,15 +263,13 @@ fn main() -> Result<()> {
                     code: KeyCode::Right,
                     modifiers: _,
                 } => {
-                    let current_line = line_buffer
-                        .line_at(cursor.row)
-                        .expect("Missing expected line.");
-                    if cursor.column < current_line.character_buffer.len() {
+                    let current_line = text_buffer.line_at(cursor.row);
+                    if cursor.column < current_line.content.len() {
                         cursor.moved(CursorMove {
                             vertical: None,
                             horizontal: Some(HorizontalMove::Right(1)),
                         });
-                    } else if cursor.row < line_buffer.lines.len() - 1 {
+                    } else if cursor.row < text_buffer.line_count() - 1 {
                         let column_delta = 0 - (cursor.column as isize);
                         let cursor_move = CursorMove {
                             vertical: Some(VerticalMove::Down(1)),
@@ -378,7 +287,7 @@ fn main() -> Result<()> {
     terminal::disable_raw_mode()
 }
 
-fn render(screen: &mut impl Write, line_buffer: &LineBuffer, cursor: &Cursor, window: &mut Window) {
+fn render(screen: &mut impl Write, text_buffer: &impl TextBuffer, cursor: &Cursor, window: &mut Window) {
     queue!(screen, Clear(ClearType::All));
 
     let (terminal_width, terminal_height) =
@@ -386,20 +295,17 @@ fn render(screen: &mut impl Write, line_buffer: &LineBuffer, cursor: &Cursor, wi
 
     // Number of columns the display of line numbers will require: max(3, num_digits) + 1 space
     let min_line_number_columns = 3usize;
-    let line_number_digits = line_buffer.lines.len().to_string().len();
+    let line_number_digits = text_buffer.line_count().to_string().len();
     let line_number_columns =
         (std::cmp::max(min_line_number_columns, line_number_digits) + 1) as u16;
 
     window.resize(terminal_height, terminal_width - line_number_columns);
     window.update_offsets_for_cursor(cursor);
 
-    let line_iter = line_buffer
-        .lines
-        .iter()
-        .enumerate()
-        .skip(window.vertical_offset)
-        .take(window.height as usize);
-
+    let last_line= std::cmp::min(window.bottom(), text_buffer.line_count());
+    let line_range = window.vertical_offset..last_line;
+    let line_iter = line_range.map(|x| (x, text_buffer.line_at(x)));
+    
     let mut row_count = 0;
     for (row_index, line) in line_iter {
         queue!(
@@ -416,7 +322,7 @@ fn render(screen: &mut impl Write, line_buffer: &LineBuffer, cursor: &Cursor, wi
         );
 
         let character_iter = line
-            .character_buffer
+            .content
             .iter()
             .skip(window.horizontal_offset)
             .take(window.width as usize);
